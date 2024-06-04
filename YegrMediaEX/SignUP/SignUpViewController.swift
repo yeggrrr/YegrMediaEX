0a89d31682a4a6dea914681fef36365d93124b1d
yeggrrr <yjkim981103@gmail.com>
2024-06-04 16:25:44 +0900

    Initial Commit

---
+++ YegrMediaEX/SignUP/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  YegrMediaEX
+//
+//  Created by YJ on 6/4/24.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+}
---
+++ YegrMediaEX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YegrMediaEX
+//
+//  Created by YJ on 6/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
